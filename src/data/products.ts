3dd4f3d0bbf4404850501dbbfcac31dad689878c
Basim Almatboli <47425179+BasimAlmatboli@users.noreply.github.com>
2025-04-29 11:38:39 +0300

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -2,15 +2,15 @@ import { Product } from '../types';
 
 const defaultProducts: Product[] = [
   {
-    id: 'rgb-light',
-    name: 'Lines RGB Light',
+    id: 'old-rgb-light',
+    name: 'Old RGB Light',
     cost: 130,
     sellingPrice: 199,
     owner: 'yassir'  // Yassir's product
   },
   {
     id: 'new-rgb-light',
-    name: 'NEW Lines RGB Light',
+    name: 'New RGB Light',
     cost: 130,
     sellingPrice: 215,
     owner: 'yassir'  // Yassir's product
@@ -23,33 +23,62 @@ const defaultProducts: Product[] = [
     owner: 'yassir'  // Yassir's product
   },
   {
+    id: 'pixel-16',
+    name: 'pixel 16',
+    cost: 80,
+    sellingPrice: 193,
+    owner: 'basim'  // Basim's product
+  },
+  {
+    id: 'pixel-32',
+    name: 'pixel 32',
+    cost: 95,
+    sellingPrice: 248,
+    owner: 'basim'  // Basim's product
+  },
+ {
+    id: 'pixel-64',
+    name: 'pixel 64',
+    cost: 220,
+    sellingPrice: 349,
+    owner: 'basim'  // Basim's product
+  },
+  {
+    id: 'PC-Light-24',
+    name: 'PC Light 24',
+    cost: 55,
+    sellingPrice: 179,
+    owner: 'basim'  // Basim's product
+  },
+  {
+    id: 'PC-Light-27',
+    name: 'PC Light 27',
+    cost: 58,
+    sellingPrice: 189,
+    owner: 'basim'  // Basim's product
+  },
+  {
+    id: 'PC-Light-32',
+    name: 'PC Light 32',
+    cost: 60,
+    sellingPrice: 194,
+    owner: 'basim'  // Basim's product
+  },
+  {
+    id: 'PC-Light-34',
+    name: 'PC Light 34',
+    cost: 61,
+    sellingPrice: 199,
+    owner: 'basim'  // Basim's product
+  },
+
+  {
     id: 'smart-cube',
     name: 'Smart Cube',
     cost: 77,
     sellingPrice: 149,
     owner: 'basim'  // Basim's product
   }, 
-  {
-    id: 'pixel-screen-16',
-    name: 'pixel screen 16',
-    cost: 80,
-    sellingPrice: 193,
-    owner: 'basim'  // Basim's product
-  },
-  {
-    id: 'pixel-screen-32',
-    name: 'pixel screen 32',
-    cost: 95,
-    sellingPrice: 248,
-    owner: 'basim'  // Basim's product
-  },
-  {
-    id: 'ٍSmart-PC-Light',
-    name: 'ٍSmart PC Light',
-    cost: 85,
-    sellingPrice: 179,
-    owner: 'basim'  // Basim's product
-  },
   {
     id: 'NEON ROPE',
     name: 'NEON ROPE',
