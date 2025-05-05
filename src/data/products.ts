0c0fe399c24d771b83efb4b8f1145fb418a7846c
Basim Almatboli <47425179+BasimAlmatboli@users.noreply.github.com>
2025-05-05 11:18:28 +0300

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -2,70 +2,70 @@ import { Product } from '../types';
 
 const defaultProducts: Product[] = [
   {
-    id: 'old-rgb-light',
+    id: 'Old RGB Light',
     name: 'Old RGB Light',
     cost: 130,
     sellingPrice: 199,
     owner: 'yassir'  // Yassir's product
   },
   {
-    id: 'new-rgb-light',
+    id: 'New RGB Light',
     name: 'New RGB Light',
     cost: 130,
     sellingPrice: 215,
     owner: 'yassir'  // Yassir's product
   },
   {
-    id: 'mousepad',
+    id: 'Mousepad',
     name: 'Mousepad',
     cost: 16,
     sellingPrice: 79,
     owner: 'yassir'  // Yassir's product
   },
   {
-    id: 'pixel-16',
+    id: 'pixel 16',
     name: 'pixel 16',
     cost: 80,
     sellingPrice: 193,
     owner: 'basim'  // Basim's product
   },
   {
-    id: 'pixel-32',
+    id: 'pixel 32',
     name: 'pixel 32',
     cost: 95,
     sellingPrice: 248,
     owner: 'basim'  // Basim's product
   },
  {
-    id: 'pixel-64',
+    id: 'pixel 64',
     name: 'pixel 64',
     cost: 220,
     sellingPrice: 349,
     owner: 'basim'  // Basim's product
   },
   {
-    id: 'PC-Light-24',
+    id: 'PC Light 24',
     name: 'PC Light 24',
     cost: 55,
     sellingPrice: 179,
     owner: 'basim'  // Basim's product
   },
   {
-    id: 'PC-Light-27',
+    id: 'PC Light 27',
     name: 'PC Light 27',
     cost: 58,
     sellingPrice: 189,
     owner: 'basim'  // Basim's product
   },
   {
-    id: 'PC-Light-32',
+    id: 'PC Light 32',
     name: 'PC Light 32',
     cost: 60,
     sellingPrice: 194,
     owner: 'basim'  // Basim's product
   },
   {
-    id: 'PC-Light-34',
+    id: 'PC Light 34',
     name: 'PC Light 34',
     cost: 61,
     sellingPrice: 199,
@@ -73,7 +73,7 @@ const defaultProducts: Product[] = [
   },
 
   {
-    id: 'smart-cube',
+    id: 'Smart Cube',
     name: 'Smart Cube',
     cost: 77,
     sellingPrice: 149,
